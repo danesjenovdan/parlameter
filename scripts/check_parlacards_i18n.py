a2683ce7b4194aad3f0684b329ced51099f796de
LepkoQQ <lepko.san@gmail.com>
2025-05-09 16:24:59 +0200

    improve check parlacards i18n script to check nested yaml keys

--- scripts/check_parlacards_i18n.py
+++ scripts/check_parlacards_i18n.py
@@ -1,25 +1,15 @@
-import os
-import yaml
-from pathlib import Path
 import glob
-from collections import Counter
 import itertools
+import sys
+import os
+from collections import Counter
+from pathlib import Path
+
+from get_yaml_keys import get_yaml_keys
 
 ###########################################################################
 # Output to file with `python check_parlacards_i18n.py > i18n_report.txt` #
 ###########################################################################
-
-
-def get_yaml_keys(filepath):
-    with open(filepath, "r") as f:
-        if data := yaml.safe_load(f):
-            return list(data.keys())
-
-    return []
-
-
-LOCALES = [path.split("/")[-1] for path in glob.glob("parlacards/cards/_i18n/*")]
-LOCALES.remove("d3locales.js")
 
 
 def box_print(text, box="#"):
@@ -54,6 +44,8 @@ def report_missing_files(locales):
 
     all_files = set(itertools.chain(*locale_paths.values()))
 
+    ret = False
+
     for locale in locales:
         this_locale_paths = set(locale_paths[locale])
         if this_locale_paths != all_files:
@@ -62,6 +54,9 @@ def report_missing_files(locales):
             for missing_file in missing_files:
                 print(missing_file)
             print()
+            ret = True
+
+    return ret
 
 
 def report_missing_keys(locales):
@@ -88,26 +83,53 @@ def report_missing_keys(locales):
                     get_yaml_keys(f"parlacards/cards/_i18n/{locale}/{locale_file_path}")
                 )
 
-    all_files = set(itertools.chain(*file_paths_in_locale.values()))
-    all_files_check = set(all_keys_by_file.keys())
+    ret = False
+    missing_keys_count = Counter()
 
     for locale in locales:
-        box_print(locale, "+")
+        box_printed = False
         for locale_file_path in file_paths_in_locale[locale]:
             locale_keys = get_yaml_keys(
                 f"parlacards/cards/_i18n/{locale}/{locale_file_path}"
             )
             if set(locale_keys) != all_keys_by_file[locale_file_path]:
+                # print the locale title box only once on first occurrence
+                if not box_printed:
+                    box_print(locale, "+")
+                    box_printed = True
+
                 underline_print(locale_file_path, "-")
+
                 for key in all_keys_by_file[locale_file_path] - set(locale_keys):
                     print(key)
+                    missing_keys_count[locale_file_path] += 1
                 print()
+
+                ret = True
+
+    if ret:
+        print("Total missing keys:")
+        for file_path, count in missing_keys_count.items():
+            print(f"{file_path}: {count} missing keys")
+
+    return ret
 
 
 if __name__ == "__main__":
+    dir_path = Path(__file__).parent.parent.resolve()
+    os.chdir(dir_path)
+
+    LOCALES = [path.split("/")[-1] for path in glob.glob("parlacards/cards/_i18n/*")]
+    LOCALES.remove("d3locales.js")
+
+    exit_with_error_code = False
+
     box_print("MISSING FILES REPORT")
-    report_missing_files(LOCALES)
+    exit_with_error_code |= report_missing_files(LOCALES)
+
     print()
 
     box_print("MISSING KEYS REPORT")
-    report_missing_keys(LOCALES)
+    exit_with_error_code |= report_missing_keys(LOCALES)
+
+    sys.exit(int(exit_with_error_code))
