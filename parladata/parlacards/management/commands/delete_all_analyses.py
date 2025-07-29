cfb2e0ebba11731098cfcd382a59afae59c79cc3
LepkoQQ <lepko.san@gmail.com>
2025-07-29 16:34:58 +0200

    update test db, fix group unity tests and update instructions on how to change the test database

---
+++ parladata/parlacards/management/commands/delete_all_analyses.py
@@ -0,0 +1,37 @@
+from django.core.management.base import BaseCommand
+
+from parlacards.models import *
+
+ALL_MODELS = [
+    PersonVocabularySize,
+    GroupVocabularySize,
+    VotingDistance,
+    GroupVotingDistance,
+    PersonAvgSpeechesPerSession,
+    DeviationFromGroup,
+    PersonNumberOfQuestions,
+    PersonMonthlyVoteAttendance,
+    GroupMonthlyVoteAttendance,
+    GroupNumberOfQuestions,
+    PersonVoteAttendance,
+    GroupVoteAttendance,
+    PersonStyleScore,
+    GroupStyleScore,
+    PersonNumberOfSpokenWords,
+    PersonTfidf,
+    GroupTfidf,
+    SessionTfidf,
+    SessionGroupAttendance,
+    GroupDiscord,
+    Quote,
+    GroupUnity,
+]
+
+
+class Command(BaseCommand):
+    help = "Deletes all analyses"
+
+    def handle(self, *args, **options):
+        for model in ALL_MODELS:
+            model.objects.all().delete()
+        self.stdout.write(self.style.SUCCESS("Successfully deleted all analyses."))
