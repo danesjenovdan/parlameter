ba15b7f43a356e3a8aca179d9c98d13c564a852e
Tomaž Kunst <tomazkunst@gmail.com>
2026-03-10 14:03:25 +0100

    fix data tests for production run

--- parladata/parladata/data_tests.py
+++ parladata/parladata/data_tests.py
@@ -2,16 +2,22 @@ from django.conf import settings
 from django.contrib.auth.models import Group
 from django.db.models import Count
 
-from parladata.models import Mandate, Session, Speech, Vote
+from parladata.models import Mandate, Session, Speech, Vote, Person
 from parladata.update_utils import send_email
+
+from parlacards.utils import get_playing_fields
+
+from datetime import datetime
 
 
 def check_for_duplicated_sessions():
     """
     Test whether there are sessions of the same organization with the same name
     """
+    timestamp = datetime.now()
     duplicated_sessions = (
-        Session.objects.values("name", "organizations")
+        Session.objects.filter(mandate=2)
+        .values("name", "organizations")
         .annotate(same_name=Count("name"))
         .filter(same_name__gt=1)
     )
@@ -22,8 +28,11 @@ def check_for_duplicated_votes():
     """
     Test whether there are vote with the same name and timestamp
     """
+    timestamp = datetime.now()
+    pfs = get_playing_fields(timestamp)
     duplicated_votes = (
-        Vote.objects.values("name", "timestamp")
+        Vote.objects.filter(motion__session__organizations__in=pfs)
+        .values("name", "timestamp")
         .annotate(same_name=Count("name"))
         .filter(same_name__gt=1)
     )
@@ -34,18 +43,16 @@ def check_num_of_ballots_per_vote():
     """
     Test whether there is a vote where there is no correct number of ballots in relation to the number of voters.
     """
+    timestamp = datetime.now()
+    pfs = get_playing_fields(timestamp)
     invalid_votes = []
-    vv = Vote.objects.all()
-    for v in vv:
-        try:
-            org = v.motion.session.organizations.first()
-        except:
-            invalid_votes.append(v)
-            continue
-        number_of_voters = org.number_of_voters_at(timestamp=v.timestamp)
-        count = v.ballots.count()
-        if count != number_of_voters:
-            invalid_votes.append(v)
+    for pf in pfs:
+        vv = Vote.objects.filter(motion__session__organizations=pf).distinct()
+        for v in vv:
+            number_of_voters = pf.number_of_voters_at(timestamp=v.timestamp)
+            count = v.ballots.count()
+            if count != number_of_voters:
+                invalid_votes.append((v, count, number_of_voters))
     return invalid_votes
 
 
@@ -82,7 +89,7 @@ def get_session_with_duplicated_speeches():
 def run_tests():
     duplicated_sessions = check_for_duplicated_sessions()
     duplicated_votes = check_for_duplicated_votes()
-    invalid_votes = check_num_of_ballots_per_vote()
+    invalid_ballots = check_num_of_ballots_per_vote()
     sessions_with_duplicated_speeches = get_session_with_duplicated_speeches()
 
     parser_permission_group = Group.objects.filter(
@@ -94,7 +101,7 @@ def run_tests():
     if (
         duplicated_sessions
         or duplicated_votes
-        or invalid_votes
+        or invalid_ballots
         or sessions_with_duplicated_speeches
     ):
         for parser_owner in parser_permission_group.user_set.all():
@@ -106,7 +113,7 @@ def run_tests():
                     "base_url": settings.BASE_URL,
                     "duplicated_sessions": duplicated_sessions,
                     "duplicated_votes": duplicated_votes,
-                    "invalid_votes": invalid_votes,
+                    "invalid_ballots": invalid_ballots,
                     "sessions_with_duplicated_speeches": sessions_with_duplicated_speeches,
                 },
             )
